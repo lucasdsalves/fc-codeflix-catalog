d51632c3d6137da4ecc1541755beabf88d3d7205
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-09-05 18:47:46 -0300

    test: add create category test fixture

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,44 +1,91 @@
-﻿using CreateCategoryUseCase = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using CreateCategoryUseCase = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory
 {
+    [Collection(nameof(CreateCategoryTestFixture))]
     public class CreateCategoryTest
     {
+        private readonly CreateCategoryTestFixture _fixture;
+        private readonly Mock<ICategoryRepository> _repositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        public CreateCategoryTest(CreateCategoryTestFixture fixture)
+        {
+            _fixture = fixture;
+            _repositoryMock = new Mock<ICategoryRepository>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+        }
+
         [Fact(DisplayName = nameof(CreateCategory))]
         [Trait("Application", "CreateCategory - Use Cases")]
         public async void CreateCategory()
         {
             // Arrange
-            var repositoryMock = new Mock<ICategoryRepository>();
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var useCase = new CreateCategoryUseCase.CreateCategory(_repositoryMock.Object, _unitOfWorkMock.Object);
 
-            var useCase = new CreateCategoryUseCase.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
-
-            var input = new CreateCategoryUseCase.CreateCategoryInput(
-                "Category Name",
-                "Category Description",
-                true);
+            var input = _fixture.GetInput();
 
             // Act
             var output = await useCase.Handle(input, CancellationToken.None);
 
             // Assert
-            repositoryMock.Verify(repository => 
+            _repositoryMock.Verify(repository =>
                                              repository.Insert(
                                                             It.IsAny<Category>(),
-                                                            It.IsAny<CancellationToken>()), 
+                                                            It.IsAny<CancellationToken>()),
                                                             Times.Once);
 
-            unitOfWorkMock.Verify(uow => 
+            _unitOfWorkMock.Verify(uow =>
                                         uow.Commit(
                                             It.IsAny<CancellationToken>()),
                                             Times.Once);
 
             output.Should().NotBeNull();
             (output.Id != Guid.Empty).Should().BeTrue();
-            output.Name.Should().Be("Category Name");
-            output.Description.Should().Be("Category Description");
-            output.IsActive.Should().BeTrue();  
+            output.Name.Should().Be(input.Name);
+            output.Description.Should().Be(input.Description);
+            output.IsActive.Should().Be(input.IsActive);
+        }
+
+        [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregate))]
+        [Trait("Application", "CreateCategory - Use Cases")]
+        [MemberData(nameof(GetInvalidInputs))]
+        public async void ThrowWhenCantInstantiateAggregate(CreateCategoryInput input, string exceptionMessage)
+        {
+            var useCase = new CreateCategoryUseCase.CreateCategory(_repositoryMock.Object, _unitOfWorkMock.Object);
+
+            Func<Task> task = async () => await useCase.Handle(input, CancellationToken.None);
+
+            await task.Should()
+                      .ThrowAsync<EntityValidationException>()
+                      .WithMessage(exceptionMessage);
+        }
+
+        public static IEnumerable<object[]> GetInvalidInputs()
+        {
+            var fixture = new CreateCategoryTestFixture();
+            var invalidInputList = new List<object[]>();
+
+            var invalidInputShortName = fixture.GetInput();
+            invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+
+            invalidInputList.Add(new object[]
+            {
+                invalidInputShortName,
+                "Name should not be less than 3 characters long"
+            });
+
+            var invalidInputDescriptionNull = fixture.GetInput();
+            invalidInputDescriptionNull.Description = null!;
+
+            invalidInputList.Add(new object[]
+            {
+                invalidInputDescriptionNull,
+                "Description should not be null or empty"
+            });
+
+            return invalidInputList;
         }
     }
 }
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.UnitTests.Common;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory
+{
+    [CollectionDefinition(nameof(CreateCategoryTestFixture))]
+    public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> {}
+
+    public class CreateCategoryTestFixture : BaseFixture
+    {
+        public CreateCategoryTestFixture(): base() { }
+
+        public CreateCategoryInput GetInput()
+            => new(
+                GetValidCategoryName(),
+                GetValidCategoryDescription(),
+                GetRandomBoolean()
+                );
+
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+
+            if (categoryName.Length > 255)
+                categoryName = categoryName.Substring(0, 255);
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription = Faker.Commerce.ProductDescription();
+
+            if (categoryDescription.Length > 10_000)
+                categoryDescription = categoryDescription.Substring(0, 10_000);
+
+            return categoryDescription;
+        }
+
+        public bool GetRandomBoolean()
+        {
+            return new Random().NextDouble() < 0.5;
+        }              
+    }
+}
