77221ca57973d33c0d6c12abd977c3a873db2992
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-09-11 18:23:22 -0300

    feat: add fluent validation to get category input

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,50 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory
+{
+    [Collection(nameof(GetCategoryTestFixture))]
+    public class GetCategoryInputValidatorTest
+    {
+        private readonly GetCategoryTestFixture _fixture;
+
+        public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact(DisplayName = nameof(ValidationOk))]
+        [Trait("Application", "GetCategoryInputValidator - Use Cases")]
+        public void ValidationOk()
+        {
+            // Arrange
+            var validInput = new GetCategoryInput(Guid.NewGuid());
+            var validator = new GetCategoryInputValidator();
+
+            // Act
+            var validationResult = validator.Validate(validInput);
+
+            // Assert
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeTrue();
+            validationResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = nameof(InvalidWhenEmptyGuid))]
+        [Trait("Application", "GetCategoryInputValidator - Use Cases")]
+        public void InvalidWhenEmptyGuid()
+        {
+            // Arrange
+            var invalidInput = new GetCategoryInput(Guid.Empty);
+            var validator = new GetCategoryInputValidator();
+
+            // Act
+            var validationResult = validator.Validate(invalidInput);
+
+            // Assert
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeFalse();
+            validationResult.Errors.Should().HaveCount(1);
+            validationResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+        }
+    }
+}
