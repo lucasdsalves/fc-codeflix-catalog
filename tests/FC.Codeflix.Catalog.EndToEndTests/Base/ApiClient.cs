8e7dd55c25e8cbc5e30d799e91d01fde3b415aa7
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-10-03 16:12:23 -0300

    feat: initial api project and category controller

--- tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Base/ApiClient.cs
@@ -13,19 +13,27 @@ namespace FC.Codeflix.Catalog.EndToEndTests.Base
         }
 
         public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload)
+            where TOutput : class
         {
             var response = await _httpClient.PostAsync(route, new StringContent(
                                                                   JsonSerializer.Serialize(payload), 
                                                                   Encoding.UTF8, 
                                                                   "application/json"));
 
-            var outputString  = await response.Content.ReadAsStringAsync(); 
+            var outputString  = await response.Content.ReadAsStringAsync();
 
-            var output = JsonSerializer.Deserialize<TOutput>
+            TOutput? output = null;
+
+            if (!string.IsNullOrWhiteSpace(outputString))
+            {
+                output = JsonSerializer.Deserialize<TOutput>
                                         (outputString,
-                                        new JsonSerializerOptions { 
-                                                PropertyNameCaseInsensitive = true});
-            
+                                        new JsonSerializerOptions
+                                        {
+                                            PropertyNameCaseInsensitive = true
+                                        });
+            }
+           
             return (response, output);
         }
     }
